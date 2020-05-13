4fc839586f3ef85ab8ad08bea3c58d89e5453684
Jake Gealer <jake@gealer.email>
2020-05-14 00:59:32 +0100

    Add UIntTransformer

--- transformers.go
+++ transformers.go
@@ -7,11 +7,20 @@ func StringTransformer(_ *Context, Arg string) (interface{}, error) {
 	return Arg, nil
 }
 
-// IntTransformer is used to transform an arg to a number if possible.
+// IntTransformer is used to transform an arg to a integer if possible.
 func IntTransformer(_ *Context, Arg string) (interface{}, error) {
 	i, err := strconv.Atoi(Arg)
 	if err != nil {
 		return nil, &InvalidTransformation{Description: "Could not transform the argument to an integer."}
+	}
+	return i, nil
+}
+
+// IntTransformer is used to transform an arg to a unsigned integer if possible.
+func UIntTransformer(_ *Context, Arg string) (interface{}, error) {
+	i, err := strconv.ParseUint(Arg, 10, 64)
+	if err != nil {
+		return nil, &InvalidTransformation{Description: "Could not transform the argument to an unsigned integer."}
 	}
 	return i, nil
 }
