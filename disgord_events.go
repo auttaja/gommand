476caeaca781ba710f97ea14fb1135a942011399
Jake Gealer <jake@gealer.email>
2020-05-14 11:48:19 +0100

    Move processor to its own function, add unit test, add benchmark

--- disgord_events.go
+++ disgord_events.go
@@ -6,6 +6,10 @@ import (
 	"strings"
 )
 
+// Defines if the member should be patched in.
+// This is true by default, and should only be made false for unit tests.
+var patchMember = true
+
 // Handles setting the bot user initially.
 func (r *Router) readyEvt(_ disgord.Session, evt *disgord.Ready) {
 	// Set the bot user.
@@ -26,6 +30,104 @@ func (r *Router) userUpdate(_ disgord.Session, evt *disgord.UserUpdate) {
 	r.cmdLock.Lock()
 	r.BotUser = evt.User
 	r.cmdLock.Unlock()
+}
+
+// Does the message command processing.
+func (r *Router) msgCmdProcessor(s disgord.Session, evt *disgord.MessageCreate) {
+	// If the message is from a bot or this isn't in a guild, ignore it.
+	if evt.Message.Author.Bot || evt.Message.IsDirectMessage() {
+		return
+	}
+
+	// Read lock the commands.
+	r.cmdLock.RLock()
+
+	// Defer read unlocking commands.
+	defer r.cmdLock.RUnlock()
+
+	// Create the context.
+	ctx := &Context{
+		Message:          evt.Message,
+		BotUser:          r.BotUser,
+		Router:           r,
+		Session:          s,
+		Args:             []interface{}{},
+		MiddlewareParams: map[string]interface{}{},
+	}
+
+	// Create a string iterator of the message content.
+	reader := &StringIterator{Text: evt.Message.Content}
+
+	// Run a prefix check.
+	if !r.PrefixCheck(ctx, reader) {
+		// The prefix was not used.
+		return
+	}
+
+	// The member should be patched into the message object here to make it easier.
+	if patchMember {
+		member, err := s.GetMember(context.TODO(), evt.Message.GuildID, evt.Message.Author.ID)
+		if err != nil {
+			r.errorHandler(ctx, err)
+			return
+		}
+		member.GuildID = evt.Message.GuildID
+		evt.Message.Member = member
+	}
+
+	// Iterate the message until the space.
+	cmdname := ""
+	for {
+		b, err := reader.GetChar()
+		if err != nil || b == ' ' {
+			break
+		}
+		cmdname += string(b)
+	}
+	if cmdname == "" {
+		var ok bool
+		var err error
+		if r.CustomCommandsHandler != nil {
+			ok, err = r.CustomCommandsHandler(ctx, "", reader)
+		}
+		if err == nil {
+			if !ok {
+				r.errorHandler(ctx, &CommandBlank{err: "The command is blank."})
+			}
+		} else {
+			r.errorHandler(ctx, err)
+		}
+		return
+	}
+
+	// Get the remainder as raw arguments.
+	remainder, _ := reader.GetRemainder(false)
+	ctx.RawArgs = remainder
+
+	// Get the command if it exists.
+	cmd, _ := r.cmds[strings.ToLower(cmdname)]
+	ctx.Command = cmd
+	if cmd == nil {
+		var ok bool
+		var err error
+		if r.CustomCommandsHandler != nil {
+			ok, err = r.CustomCommandsHandler(ctx, cmdname, reader)
+		}
+		if err == nil {
+			if !ok {
+				r.errorHandler(ctx, &CommandBlank{err: "The command \"" + cmdname + "\" does not exist."})
+			}
+		} else {
+			r.errorHandler(ctx, err)
+		}
+		return
+	}
+
+	// Run the command handler.
+	err := cmd.run(ctx, reader)
+	if err != nil {
+		r.errorHandler(ctx, err)
+	}
 }
 
 // Handles processing new messages.
@@ -59,100 +161,7 @@ func (r *Router) msgCreate(s disgord.Session, evt *disgord.MessageCreate) {
 	}()
 
 	// Launch the handler in a go-routine.
-	go func() {
-		// If the message is from a bot or this isn't in a guild, ignore it.
-		if evt.Message.Author.Bot || evt.Message.IsDirectMessage() {
-			return
-		}
-
-		// Read lock the commands.
-		r.cmdLock.RLock()
-
-		// Defer read unlocking commands.
-		defer r.cmdLock.RUnlock()
-
-		// Create the context.
-		ctx := &Context{
-			Message:          evt.Message,
-			BotUser:          r.BotUser,
-			Router:           r,
-			Session:          s,
-			Args:             []interface{}{},
-			MiddlewareParams: map[string]interface{}{},
-		}
-
-		// Create a string iterator of the message content.
-		reader := &StringIterator{Text: evt.Message.Content}
-
-		// Run a prefix check.
-		if !r.PrefixCheck(ctx, reader) {
-			// The prefix was not used.
-			return
-		}
-
-		// The member should be patched into the message object here to make it easier.
-		member, err := s.GetMember(context.TODO(), evt.Message.GuildID, evt.Message.Author.ID)
-		if err != nil {
-			r.errorHandler(ctx, err)
-			return
-		}
-		member.GuildID = evt.Message.GuildID
-		evt.Message.Member = member
-
-		// Iterate the message until the space.
-		cmdname := ""
-		for {
-			b, err := reader.GetChar()
-			if err != nil || b == ' ' {
-				break
-			}
-			cmdname += string(b)
-		}
-		if cmdname == "" {
-			var ok bool
-			var err error
-			if r.CustomCommandsHandler != nil {
-				ok, err = r.CustomCommandsHandler(ctx, "", reader)
-			}
-			if err == nil {
-				if !ok {
-					r.errorHandler(ctx, &CommandBlank{err: "The command is blank."})
-				}
-			} else {
-				r.errorHandler(ctx, err)
-			}
-			return
-		}
-
-		// Get the remainder as raw arguments.
-		remainder, _ := reader.GetRemainder(false)
-		ctx.RawArgs = remainder
-
-		// Get the command if it exists.
-		cmd, _ := r.cmds[strings.ToLower(cmdname)]
-		ctx.Command = cmd
-		if cmd == nil {
-			var ok bool
-			var err error
-			if r.CustomCommandsHandler != nil {
-				ok, err = r.CustomCommandsHandler(ctx, cmdname, reader)
-			}
-			if err == nil {
-				if !ok {
-					r.errorHandler(ctx, &CommandBlank{err: "The command \"" + cmdname + "\" does not exist."})
-				}
-			} else {
-				r.errorHandler(ctx, err)
-			}
-			return
-		}
-
-		// Run the command handler.
-		err = cmd.run(ctx, reader)
-		if err != nil {
-			r.errorHandler(ctx, err)
-		}
-	}()
+	go r.msgCmdProcessor(s, evt)
 }
 
 // Hook is used to hook all required events into the disgord client.
